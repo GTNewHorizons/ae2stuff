931e70d758cde91193010a194e619e1ead14d32a
lordIcocain <lordicocain@gmail.com>
2024-12-06 12:17:02 +0200

    custom texture + color pickup

--- src/main/scala/net/bdew/ae2stuff/Recipes.scala
+++ src/main/scala/net/bdew/ae2stuff/Recipes.scala
@@ -19,7 +19,131 @@ object Recipes {
     )
     GameRegistry.addShapelessRecipe(
       new ItemStack(BlockWireless),
-      new ItemStack(BlockWireless, 1, OreDictionary.WILDCARD_VALUE)
+      new ItemStack(BlockWireless, 1, 1)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 2)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 3)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 4)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 5)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 6)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 7)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 8)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 9)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 10)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 11)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 12)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 13)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 14)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 15)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless),
+      new ItemStack(BlockWireless, 1, 16)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 18)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 19)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 20)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 21)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 22)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 23)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 24)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 25)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 26)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 27)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 28)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 29)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 30)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 31)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 32)
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(BlockWireless, 1, 17),
+      new ItemStack(BlockWireless, 1, 33)
     )
   }
 }
--- src/main/scala/net/bdew/ae2stuff/machines/wireless/BlockWireless.scala
+++ src/main/scala/net/bdew/ae2stuff/machines/wireless/BlockWireless.scala
@@ -17,7 +17,7 @@ import cpw.mods.fml.relauncher.{Side, SideOnly}
 import net.bdew.ae2stuff.misc.{BlockWrenchable, MachineMaterial}
 import net.bdew.lib.Misc
 import net.bdew.lib.block.{HasItemBlock, HasTE, ItemBlockTooltip, SimpleBlock}
-import net.minecraft.block.Block
+import net.minecraft.block.{Block, BlockContainer}
 import net.minecraft.client.renderer.texture.IIconRegister
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.entity.EntityLivingBase
@@ -50,7 +50,11 @@ object BlockWireless
     val stack = new ItemStack(this)
     val te = getTE(world, x, y, z)
     if (te.isHub) {
-      stack.setItemDamage(17)
+      if (te.color != AEColor.Transparent) {
+        stack.setItemDamage(te.color.ordinal() + 18)
+      } else {
+        stack.setItemDamage(17)
+      }
     } else if (te.color != AEColor.Transparent) {
       stack.setItemDamage(te.color.ordinal() + 1)
     }
@@ -64,7 +68,7 @@ object BlockWireless
       tab: CreativeTabs,
       list: util.List[_]
   ): Unit = {
-    for (meta <- 0 to 17) {
+    for (meta <- 0 to 33) {
       list
         .asInstanceOf[util.List[ItemStack]]
         .add(new ItemStack(itemIn, 1, meta))
@@ -82,7 +86,11 @@ object BlockWireless
     val stack = new ItemStack(this)
     val te = getTE(world, x, y, z)
     if (te.isHub) {
-      stack.setItemDamage(17)
+      if (te.color != AEColor.Transparent) {
+        stack.setItemDamage(te.color.ordinal() + 18)
+      } else {
+        stack.setItemDamage(17)
+      }
     } else if (te.color != AEColor.Transparent) {
       stack.setItemDamage(te.color.ordinal() + 1)
     }
@@ -118,8 +126,13 @@ object BlockWireless
       if (stack.hasDisplayName) {
         te.customName = stack.getDisplayName
       }
-      if (itemDamage == 17) {
+      if (itemDamage > 16) {
         te.isHub = true;
+        if (itemDamage == 17) {
+          te.color = AEColor.values().apply(16)
+        } else {
+          te.color = AEColor.values().apply(itemDamage - 18)
+        }
       } else if (itemDamage > 0) {
         te.color = AEColor.values().apply(itemDamage - 1)
       }
@@ -168,10 +181,18 @@ object BlockWireless
   ): IIcon = {
     val te = getTE(worldIn, x, y, z)
     val meta = worldIn.getBlockMetadata(x, y, z)
-    if (meta > 0) {
-      icon_on.apply(te.color.ordinal())
+    if (te.isHub) {
+      if (meta > 0) {
+        icon_on.apply(te.color.ordinal() + 17)
+      } else {
+        icon_off.apply(te.color.ordinal() + 17)
+      }
     } else {
-      icon_off.apply(te.color.ordinal())
+      if (meta > 0) {
+        icon_on.apply(te.color.ordinal())
+      } else {
+        icon_off.apply(te.color.ordinal())
+      }
     }
   }
 
@@ -179,6 +200,8 @@ object BlockWireless
   override def getIcon(side: Int, meta: Int): IIcon = {
     if (meta == 0) {
       icon_on.apply(AEColor.Transparent.ordinal())
+    } else if (meta == 17) {
+      icon_on.apply(33)
     } else {
       icon_on.apply(meta - 1)
     }
@@ -186,7 +209,7 @@ object BlockWireless
 
   @SideOnly(Side.CLIENT)
   override def registerBlockIcons(reg: IIconRegister): Unit = {
-    val index = 1.to(17)
+    val index = 1.to(34)
     icon_on = index
       .map(index =>
         reg.registerIcon(Misc.iconName(modId, name, "side_on" + index))
@@ -215,8 +238,18 @@ class ItemBlockWireless(b: Block) extends ItemBlockTooltip(b) {
     if (itemDamage == 17) {
       list
         .asInstanceOf[util.List[String]]
+        .add(Misc.toLocal("tile.ae2stuff.WirelessHub.name"))
+      list
+        .asInstanceOf[util.List[String]]
+        .add(Misc.toLocal(AEColor.values().apply(16).unlocalizedName))
+    } else if (itemDamage > 16) {
+      list
+        .asInstanceOf[util.List[String]]
+        .add(Misc.toLocal("tile.ae2stuff.WirelessHub.name"))
+      list
+        .asInstanceOf[util.List[String]]
         .add(
-          Misc.toLocal("tile.ae2stuff.WirelessHub.name")
+          Misc.toLocal(AEColor.values().apply(itemDamage - 18).unlocalizedName)
         )
     } else if (itemDamage > 0) {
       list
